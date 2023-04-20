49a0856b0f0003c20d488f8c929acc2dc47b37d4
Hishaam <hishaamtchirou73@gmail.com>
2023-04-20 16:36:18 +0000

    start

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**.{html,js}",
+  ],
+  theme: {
+    colors: {
+      'bleu': '#12C698',
+      'vert': '#069205',
+      'blanc': '#FFFFFF',
+      'noire': '#000000',
+    },
+    extend: {},
+    animation: {
+      fadeIn: 'fadeIn 2s ease-in forwards',
+    },
+    keyframes: {
+      fadeIn: {
+        '0%': {
+          opacity: 0,
+        },
+        '100%': {
+          opacity: 1,
+        },
+      },
+    },
+  }
+}
+
